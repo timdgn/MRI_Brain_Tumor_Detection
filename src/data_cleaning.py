beb6ba0e35361ba2e4320d88b8e3e0323a853e68
TimDgn <timmothy86264020@gmail.com>
2024-03-10 00:46:42 +0100

    [Feature] Remove all duplicates in the dataset

---
+++ src/data_cleaning.py
@@ -0,0 +1,42 @@
+import hashlib
+from settings import *
+
+
+def compute_hash(file):
+    hasher = hashlib.md5()
+    with open(file, 'rb') as f:
+        buf = f.read()
+        hasher.update(buf)
+    return hasher.hexdigest()
+
+
+def list_files(hash_dict):
+    for data_type in ['Training', 'Testing']:
+        for label in LABELS:
+            folder_path = os.path.join(PROJECT_DIR, 'data', 'raw', data_type, label)
+            for root, dirs, files in os.walk(folder_path):
+                for file in files:
+                    if file.endswith(".jpg"):
+                        file_path = os.path.join(root, file)
+                        file_hash = compute_hash(file_path)
+                        if file_hash in hash_dict:
+                            hash_dict[file_hash].append(file_path)
+                        else:
+                            hash_dict[file_hash] = [file_path]
+
+
+def remove_duplicates(hash_dict):
+    duplicate_count = 0
+    for hash_value, file_paths in hash_dict.items():
+        if len(file_paths) > 1:
+            for file_path in file_paths[1:]:
+                print(f"Removing duplicate (hash : {hash_value}) : {file_path}")
+                os.remove(file_path)
+                duplicate_count += 1
+    print(f"Number of duplicates : {duplicate_count}")
+
+
+if __name__ == '__main__':
+    hash_dict = {}
+    list_files(hash_dict)
+    remove_duplicates(hash_dict)
